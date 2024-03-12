1d52e67e9a3ab31e42e91e69f7267f6c2de4842f
dazej <jessiadams27@gmail.com>
2024-03-12 06:21:02 -0400

    feat: get pets

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
+    //This function accepts a single parameter body that describes the body to be sent in the response.
    res.send("Homepage")
+
 });
 
 // hello world route
@@ -25,13 +27,17 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
- 
+    //want to import pets data to this file using const pets = require('./data')
+    //then send the data to the page (res.send)
+    // then turn the data into a string using json (res.json)
+    res.json(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    // target the owner key within each pet object 
+    // then send the data to the webpage
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
